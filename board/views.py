d8484798a05b26cfc46ccf1cfca204ec4787e682
kimemin <hello.kimemin@gmail.com>
2018-04-07 18:29:00 +0900

    cors 문제 해결 및 posts 조회 개발

--- board/views.py
+++ board/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.http import HttpResponse
+from django.core import serializers
 from .models import Post
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(visibleYn=True).order_by('created_date')
     return render(request, './index.html', {'posts': posts})
+
+def post_list_byJson(request):
+    posts = Post.objects.filter(visibleYn=True).order_by('created_date')
+    return HttpResponse(serializers.serialize('json', posts))
--- vue-front/src/components/Board.vue
+++ vue-front/src/components/Board.vue
@@ -1,25 +1,27 @@
 <template>
 <div>
-  <b>ajax test</b>
-  <div>{{ testText }}</div>
+  <ul id="posts">
+    <li v-for="post in posts">
+      {{ post.pk }}
+    </li>
+  </ul>
 </div>
 </template>
 
 <script>
 export default {
   name: 'board',
-  data: function() {
+  data: function () {
     return {
-      testText: ""
+      posts: []
     }
   },
   mounted: function () {
-    const testUrl = 'https://jsonplaceholder.typicode.com/posts/1'
+    const testUrl = 'http://localhost:8000/posts'
     this.$http.get(testUrl)
-      .then((result) => {
-        console.log(result.data)
-        this.testText = result.data.title
-      })
+    .then((result) => {
+      this.posts = result.data;
+    })
   }
 }
 </script>
